31de0782c6e74320bb57171b504dc38cebf9c4a0
Andrew Perelson <Andrew Perelson>
2016-10-08 18:07:27 +0800

    Added pollutantMean function.

--- AirPollution_ProgrammingAssignment.R
+++ AirPollution_ProgrammingAssignment.R
@@ -4,13 +4,33 @@ pollutantmean <- function(directory, pollutant, id=1:332) {
   
   ## 'pollutant' is a character vector of length 1 indicating
   ## the name of the pollutant for which we will calculate the 
-  ## mean; either "suflate" or "nitrate".
+  ## mean; either "sulfate" or "nitrate".
   
   ## 'id' is an integer vector indicating the monitor ID numbers
   ## to be used
   
-  ## Return the mean of the pollutant across all monitors list
+  ## Return the mean of the pollutant across all monitors listed
   ## in the 'id' vector (ignoring NA values)
   ## NOTE: Do not round the result!
   
+  substrRight <- function(x, n){
+    substr(x, nchar(x)-n+1, nchar(x))
+  }
+  
+  totalVector <- c()
+  
+  # First loop through the id's in the vector list:
+  for (fileNumber in id) {
+    theFileName <- paste(directory,substrRight(paste('00',fileNumber,sep=''),3),'.csv',sep='')
+    
+    csvData <- read.csv(file=theFileName, header=TRUE, sep=",")
+    
+    for (rowOfData in csvData[[pollutant]][!is.na(csvData[[pollutant]])]) {
+      totalVector <- c(totalVector,rowOfData)
+    }
+  }
+  
+  # Create a single vector with all the values we need so that we can get 
+  # the mean
+  mean(totalVector)
 }
