ba7e75fc195c6c7f6eda92d1d0f5176264543eae
Andrew Perelson <Andrew Perelson>
2016-10-08 18:42:07 +0800

    2 out of 3 done.

---
+++ complete.R
@@ -0,0 +1,31 @@
+complete <- function(directory, id = 1:332) {
+  
+  ## 'directory' is the location of the files
+  
+  ## id is the monitor numbers to ne used
+  
+  ## return a dataframe of the form:
+  ## id nobs
+  ## 1  117
+  ## 2 1041
+  ## ...
+  
+  substrRight <- function(x, n){
+    substr(x, nchar(x)-n+1, nchar(x))
+  }
+  
+  totalArray <- data.frame("id"=integer(),"nobs"=integer())
+  
+  # First loop through the id's in the vector list:
+  for (fileNumber in id) {
+    theFileName <- paste(directory,substrRight(paste('00',fileNumber,sep=''),3),'.csv',sep='')
+    
+    csvData <- read.csv(file=theFileName, header=TRUE, sep=",")
+    csvData <- csvData[complete.cases(csvData),]
+    
+    totalArray = rbind(totalArray, data.frame(fileNumber,nrow(csvData)))
+  }
+  
+  totalArray
+}
+
