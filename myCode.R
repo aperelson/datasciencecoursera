d9a8c8357b2f61b36588528fac68606c1da4824b
Andrew Perelson <Andrew Perelson>
2016-10-08 10:05:03 +0800

    new funcs

--- myCode.R
+++ myCode.R
@@ -6,3 +6,21 @@ myfunction <- function() {
 second <- function(x) {
   x + rnorm(length(x))
 }
+
+make.power <- function(n){
+  pow <- function(x){
+    x ^ n
+  }
+  pow
+}
+
+y <- 10
+
+f <- function(x){
+  y <- 2
+  y ^ 2 + g(x)
+}
+
+g <- function(x) {
+  x * y
+}
